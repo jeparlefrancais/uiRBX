c312984651afd778321039994243ed12f3fedb09
jeparlefrancais <jeparlefrancais21@gmail.com>
2018-02-07 01:21:38 -0500

    Action ideas added
    Basic action ideas added to the Actions list and put under categories in
    the PluginModel.

--- src/Plugin/PluginModel.lua
+++ src/Plugin/PluginModel.lua
@@ -3,11 +3,36 @@ local Actions = require(script.Parent.Actions)
 
 local Sections = {
     Global = SectionClass:New('Global', 1),
-    Style = SectionClass:New('Style', 2)
+    Properties = SectionClass:New('Properties', 2),
+    Layout = SectionClass:New('Layout', 3),
+    Text = SectionClass:New('Layout', 4),
+    Style = SectionClass:New('Style', 5)
 }
 
 -- GLOBAL SECTION
 Sections.Global:AddAction(Actions.Create)
+Sections.Global:AddAction(Actions.Convert)
+Sections.Global:AddAction(Actions.GridSize)
+Sections.Global:AddAction(Actions.ShowGrid)
+
+-- PROPERTIES
+Sections.Properties:AddAction(Actions.Position)
+Sections.Properties:AddAction(Actions.QuickPosition)
+Sections.Properties:AddAction(Actions.Size)
+Sections.Properties:AddAction(Actions.Visible)
+
+-- LAYOUT
+Sections.Layout:AddAction(Actions.List)
+Sections.Layout:AddAction(Actions.Grid)
+Sections.Layout:AddAction(Actions.Padding)
+Sections.Layout:AddAction(Actions.PageLayout)
+Sections.Layout:AddAction(Actions.AspectRatio)
+
+-- TEXT
+Sections.Text:AddAction(Actions.SetText)
+Sections.Text:AddAction(Actions.AlignText)
+Sections.Text:AddAction(Actions.SetFont)
+Sections.Text:AddAction(Actions.AddShadow)
 
 -- STYLE SECTION
 Sections.Style:AddAction(Actions.CopyStyle)

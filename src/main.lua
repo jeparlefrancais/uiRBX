5f7a129b0e15196e95f77ed43a1408641dc95b84
jeparlefrancais <jeparlefrancais21@gmail.com>
2018-02-09 19:45:50 -0500

    Open/Close plugin button added.

--- src/main.lua
+++ src/main.lua
@@ -2,20 +2,19 @@ local PluginFolder = script.Parent
 
 local ui = require(PluginFolder.UI.CreateGui)(.025, require(PluginFolder.Plugin.PluginModel))
 
+ui.Enabled = false
 ui.Parent = game:GetService('CoreGui')
 
-
---[[
 -- Setup Toolbar
 local toolbar = plugin:CreateToolbar('UI Toolkit')
  
 -- Setup button
 local button = toolbar:CreateButton(
-	'Button',
-	'Press me'
+	'UI Editor',
+	'Open/Close the UI',
+	'rbxassetid://57653546'
 )
 button.Click:Connect(function()
-
-	plugin:Activate(true) -- Neccessary to listen to mouse input
+	ui.Enabled = not ui.Enabled
 end)
- ]]
+ 

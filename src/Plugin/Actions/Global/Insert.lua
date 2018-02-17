61662819463b4e41f53706952f9f0e6679c52d99
jeparlefrancais <jeparlefrancais21@gmail.com>
2018-02-16 22:42:37 -0500

    Insert action implemented

--- src/Plugin/Actions/Global/Insert.lua
+++ src/Plugin/Actions/Global/Insert.lua
@@ -1,10 +1,29 @@
 local ActionClass = require(script.Parent.Parent.Parent.Classes.ActionClass)
 
+local InstanceList = {
+    'Frame',
+    'TextLabel',
+    'TextButton',
+    'TextBox',
+    'ImageLabel',
+    'ImageButton',
+    'ScrollingFrame',
+    'ScreenGui'
+}
+
 return ActionClass:New(
     'Trigger',
     'Insert',
     1,
     function(pluginModel)
-        pluginModel.Dialogs.Error('This functionality is not implemented yet.')
+        local parent = pluginModel.Selection:GetInstance()
+        if parent then
+            local newClass = pluginModel.Dialogs.SelectElementInList('Select a Class', InstanceList)
+            if newClass then
+                local object = Instance.new(newClass)
+                object.Parent = parent
+                pluginModel.Selection:SetInstance(object)
+            end
+        end
     end
 )

0976c70ffa216f56dd0cf6c2f00e5e1d2a43a343
jeparlefrancais <jeparlefrancais21@gmail.com>
2018-02-17 19:46:57 -0500

    Section Buttons are now draggable

--- src/UI/Utils/CreateSection.lua
+++ src/UI/Utils/CreateSection.lua
@@ -1,3 +1,5 @@
+local UIS = game:GetService('UserInputService')
+
 local Create = require(script.Parent.Create)
 local CreateActionButton = require(script.Parent.CreateActionButton)
 local SetHoverAnimation = require(script.Parent.SetHoverAnimation)
@@ -7,6 +9,7 @@ local function CreateSection(section, parent, pluginModel)
         AutoButtonColor = false,
         BackgroundColor3 = Color3.fromRGB(68, 87, 175),
         BorderSizePixel = 0,
+        Draggable = true,
         Font = Enum.Font.SourceSansSemibold,
         LayoutOrder = 2 * section:GetPosition(),
         Name = section:GetName() .. 'Button',
@@ -29,23 +32,63 @@ local function CreateSection(section, parent, pluginModel)
         Size = UDim2.new(1, 0, section:GetSize(), 0)
     }
 
+    -- make the section button draggable
+    local DragStartTime = nil
+    local DragBeginMouseLocation = nil
+    local ScreenGui = parent.Parent
+    local OriginalPosition = nil
+    local DragDelay = .2
+    local MinimalPixelDisplacement = 10
+    
+    sectionButton.DragBegin:Connect(function()
+        DragStartTime = tick()
+        DragBeginMouseLocation = UIS:GetMouseLocation()
+        OriginalPosition = parent.Position
+    end)
+
+    sectionButton.DragStopped:Connect(function()
+        DragStartTime = nil
+        DragBeginMouseLocation = nil
+        OriginalPosition = nil
+    end)
+
+    UIS.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement then
+            if DragStartTime ~= nil then
+                local mousePixelDifference = UIS:GetMouseLocation() - DragBeginMouseLocation
+                if (tick() - DragStartTime) >= DragDelay
+                    and mousePixelDifference.magnitude >= MinimalPixelDisplacement then
+                    local goalLocation = OriginalPosition + UDim2.new(
+                        mousePixelDifference.X / ScreenGui.AbsoluteSize.X,  -- Scale-X
+                        0,                                                  -- Offset-X
+                        mousePixelDifference.Y / ScreenGui.AbsoluteSize.Y,  -- Scale-Y
+                        0                                                   -- Offset-Y
+                    )
+                    parent:TweenPosition(goalLocation, Enum.EasingDirection.Out, Enum.EasingStyle.Quad, .15, true)
+                end
+            end
+        end
+    end)
+
     local Opened = true
     local ClosedSize = UDim2.new(1, 0, 0, 0)
     local OpenSize = sectionFrame.Size
     sectionButton.MouseButton1Click:Connect(function()
-        Opened = not Opened
-        sectionFrame.Visible = true
-        sectionFrame:TweenSize(Opened and OpenSize or ClosedSize,
-            Enum.EasingDirection.Out,
-            Enum.EasingStyle.Quad,
-            .2,
-            true,
-            function(status)
-                if status == Enum.TweenStatus.Completed then
-                    sectionFrame.Visible = Opened
+        if DragStartTime == nil or (UIS:GetMouseLocation() - DragBeginMouseLocation).magnitude < MinimalPixelDisplacement then
+            Opened = not Opened
+            sectionFrame.Visible = true
+            sectionFrame:TweenSize(Opened and OpenSize or ClosedSize,
+                Enum.EasingDirection.Out,
+                Enum.EasingStyle.Quad,
+                .2,
+                true,
+                function(status)
+                    if status == Enum.TweenStatus.Completed then
+                        sectionFrame.Visible = Opened
+                    end
                 end
-            end
-        )
+            )
+        end
     end)
 
     -- Create Action buttons
